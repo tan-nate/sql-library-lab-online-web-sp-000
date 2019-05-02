ee4b3cf0866556bcf30efceedc7c76e880ca2b3a
Nate Tan <nktan93@gmail.com>
2019-05-02 20:41:44 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -8,9 +8,11 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-SELECT * 
+SELECT Series.title, Characters.species
 FROM Characters INNER JOIN Series 
 ON Characters.series_id = Series.id
+GROUP BY Series.title
+ORDER BY COUNT(Characters.species);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 

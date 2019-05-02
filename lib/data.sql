728d2a2ece724d7abbc6a04240276ec2d8f61d41
Nate Tan <nktan93@gmail.com>
2019-05-02 20:29:46 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -6,6 +6,7 @@ INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Au
 
 SELECT Authors.name, Subgenres.name
 FROM Authors INNER JOIN Subgenres
+ON 
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 

570e788f7260c738c380645d613d69276c681c77
Nate Tan <nktan93@gmail.com>
2019-05-02 21:35:26 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -13,9 +13,3 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
-
-SELECT Characters.name, COUNT(Characters.id)
-FROM Character_books INNER JOIN Characters
-ON Character_books.character_id = Characters.id
-GROUP BY Characters.id
-ORDER BY COUNT(Characters.id) DESC;
